97b82c6faed8c735427c3741f0136bd5b7a46cbc
bynico1 <nicofoque0205@gmail.com>
2023-07-21 18:26:21 -0500

    improved UI

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const playerSel = document.querySelectorAll(".player-sel");
+const playerSel = document.querySelectorAll(".options");
+const roundR = document.querySelector(".round-result");
 const personC = document.querySelector(".person-counter");
 const pcCounter = document.querySelector(".pc-counter");
+
+// Start Again Section
 const result = document.querySelector(".result");
 const modal = document.querySelector(".modal");
 const startAgain = document.querySelector(".start-again");
@@ -21,7 +24,7 @@ let playerRoundWins = 0;
 let computerRoundWins = 0;
 let totalRounds = 0;
 
-function round(playerS = "rock", computerS) {
+function round(playerS, computerS) {
   playerS = playerS.toLowerCase();
   computerS = computerS.toLowerCase();
   totalRounds += 1;
@@ -31,6 +34,7 @@ function round(playerS = "rock", computerS) {
     computerRoundWins += 1;
     personC.innerText = playerRoundWins;
     pcCounter.innerText = computerRoundWins;
+    roundR.innerText = "tie";
 
     return "tie";
   } else if (
@@ -40,34 +44,38 @@ function round(playerS = "rock", computerS) {
   ) {
     computerRoundWins += 1;
     pcCounter.innerText = computerRoundWins;
-    return `You Lose! ${computerS} beats ${playerS}`;
+
+    roundR.innerText = `You Lose! ${computerS} beats ${playerS}`;
   } else {
     playerRoundWins += 1;
     personC.innerText = playerRoundWins;
-    return `You Won! ${playerS} beats ${computerS}`;
+
+    roundR.innerText = `You Won! ${playerS} beats ${computerS}`;
+    return;
+  }
+}
+function game(el) {
+  console.log(
+    round(el.currentTarget.getAttribute("data-option"), getComputerChoice())
+  );
+
+  // Winner situation
+  if (totalRounds === 5 && playerRoundWins > computerRoundWins) {
+    console.log("You Win");
+    modal.classList.toggle("hidden");
+    result.innerText = "You win";
+  } else if (totalRounds === 5 && computerRoundWins > playerRoundWins) {
+    console.log("computer win");
+    modal.classList.toggle("hidden");
+    result.innerText = "Computer win";
+  } else if (totalRounds === 5 && computerRoundWins === playerRoundWins) {
+    modal.classList.toggle("hidden");
+    result.innerText = "Game Tied";
+    console.log("game tied");
   }
 }
 
-playerSel.forEach(
-  (el) =>
-    (el.onclick = function () {
-      console.log(round(el.getAttribute("val"), getComputerChoice()));
-
-      if (totalRounds === 5 && playerRoundWins > computerRoundWins) {
-        console.log("You Win");
-        modal.classList.toggle("hidden");
-        result.innerText = "You win";
-      } else if (totalRounds === 5 && computerRoundWins > playerRoundWins) {
-        console.log("computer win");
-        modal.classList.toggle("hidden");
-        result.innerText = "Computer win";
-      } else if (totalRounds === 5 && computerRoundWins === playerRoundWins) {
-        modal.classList.toggle("hidden");
-        result.innerText = "Game Tied";
-        console.log("game tied");
-      }
-    })
-);
+playerSel.forEach((el) => el.addEventListener("click", game));
 
 startAgain.onclick = function () {
   playerRoundWins = 0;
@@ -78,7 +86,3 @@ startAgain.onclick = function () {
 
   modal.classList.toggle("hidden");
 };
-// paper.onclick = function () {
-//   console.log(round("paper", getComputerChoice()));
-
-// };
